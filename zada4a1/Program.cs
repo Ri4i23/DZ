970ef1a27b29322e85f20fc6aca0a690cfa7fee7
Ruslan <ri4i23aminov@yandex.ru>
2022-04-15 14:19:33 +0300

    zada4a1

---
+++ zada4a1/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int total = Total(number);
+
+Console.WriteLine(total);
+
+int Total(int arg)
+{
+  int first = number % 100;
+  return first / 10;
+}
+    
+
+
+
